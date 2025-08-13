c371dc975c6370cf2fa73fb743153f48ed2c69ea
Edmund Tse <edmund.tse@mobigator.com>
2025-08-13 12:44:12 +1000

    remove banner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,27 +7,27 @@ import CardDetail from "@/pages/card-detail";
 import NotFound from "@/pages/not-found";
 
 function Router() {
-  return (
-    <WouterRouter base="/prepaid-card-tracker">
-      <Switch>
-        <Route path="/" component={CardList} />
-        <Route path="/card/:id" component={CardDetail} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
-  );
+    return (
+        <WouterRouter base="/prepaid-card-tracker">
+            <Switch>
+                <Route path="/" component={CardList} />
+                <Route path="/card/:id" component={CardDetail} />
+                <Route component={NotFound} />
+            </Switch>
+        </WouterRouter>
+    );
 }
 
 function App() {
-  return (
-    <TooltipProvider>
-      <div className="max-w-md mx-auto bg-white dark:bg-gray-900 min-h-screen relative">
-        <Toaster />
-        <Router />
-        <PWAInstallBanner />
-      </div>
-    </TooltipProvider>
-  );
+    return (
+        <TooltipProvider>
+            <div className="max-w-md mx-auto bg-white dark:bg-gray-900 min-h-screen relative">
+                <Toaster />
+                <Router />
+                <PWAInstallBanner />
+            </div>
+        </TooltipProvider>
+    );
 }
 
 export default App;
